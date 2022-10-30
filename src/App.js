2724a26f05968552cbe21db3f5c976f3f4455b1d
Pinpimarn Pinpisit <pinpimarn.p@ku.th>
2022-10-30 20:21:11 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,96 +1,19 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import LogoPic from "./pics/app_logo.png";
-import Dots from "./pics/dots.png";
-import Curve from "./pics/curve.png";
-import "./styles.css";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+import Login from './Login'
+import SignUp from './SignUp'
+import Password from './Password'
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>USERNAME : </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>PASSWORD : </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
-
+const App = () => {
   return (
-    <div className="app">
-      <div className="login-topper-form"></div>
-      <div className="login-form">
-      {/* <img src={Curve} className="curve-component" height = "300px"/> */}
-      <img src={LogoPic} className="App-logo" height = "300px"/>
-      <img src={Dots} className="dot-component" height = "90px"/>
-        <div className="title">LOGIN </div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-        <br></br>
-        <div className="endingleft">FORGOT PASSWORD?</div>
-        <div className="endingleft">SIGNUP NOW</div>
-      </div>
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/SignUp" element={<SignUp/>}/>
+          <Route exact path="/Password" element={<Password/>}/>
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/SignUp.js
@@ -0,0 +1,125 @@
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom";
+// import {Alert} from "react-bootstrap"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import LogoPic from "./pics/app_logo.png";
+import Dots from "./pics/dots.png";
+import Curve from "./pics/curve.png";
+import { Link, useNavigate } from "react-router-dom"
+import "./styles.css";
+import { useAuth } from "./contexts/AuthContext"
+
+
+function SignUp() {
+  // React States
+    // const emailRef = useRef()
+    // const passwordRef = useRef()
+    // const passwordConfirmRef = useRef()
+    // const { signup } = useAuth()
+    // const [error, setError] = useState("")
+    // const [loading, setLoading] = useState(false)
+    // const history = useNavigate()
+    // const [isSubmitted, setIsSubmitted] = useState(false);
+    // React States
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+  // User Login info
+    const database = [
+    {
+        username: "user1",
+        password: "pass1"
+    },
+    {
+        username: "user2",
+        password: "pass2"
+    }
+];
+
+const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+};
+
+const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+        if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+    } else {
+        setIsSubmitted(true);
+    }
+    } else {
+      // Username not found
+        setErrorMessages({ name: "uname", message: errors.uname });
+    }
+};
+
+  // Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+    <div className="error">{errorMessages.message}</div>
+    );
+
+
+    const renderForm = (
+    <div className="form">
+    <form onSubmit={handleSubmit}>
+        {/* <div className="input-container">
+            <label>USERNAME : </label>
+            <input type="text" name="uname" placeholder="Type your username" required />
+            {renderErrorMessage("uname")}
+        </div> */}
+        <div className="input-container">
+            <label>EMAIL : </label>
+            <input type="email" placeholder="Type your email" required />
+            {/* {renderErrorMessage("email")} */}
+        </div>
+        <div className="input-container">
+            <label>PASSWORD : </label>
+            <input type="password" placeholder="Type your password" required />
+            {/* {renderErrorMessage("pass")} */}
+        </div>
+        <div className="input-container">
+            <label>PASSWORD : </label>
+            <input type="password" placeholder="Type your password again" required />
+            {/* {renderErrorMessage("pass")} */}
+        </div>
+        <div className="button-container">
+            <input type="submit" />
+        </div>
+        <div className="w-100 text-center mt-2">
+        Already have an account? <link to="/login">Log In</link>
+    </div>
+    </form>
+    </div>
+);
+
+return (
+    <div className="app">
+        <div className="login-topper-form"></div>
+        <div className="login-form">
+        {/* <img src={Curve} className="curve-component" height = "300px"/> */}
+        <a href="http://localhost:3000/login">
+            <img src={LogoPic} className="App-logo" height = "300px"/>
+        </a>
+        <img src={Dots} className="dot-component" height = "90px"/>
+            <div className="title">RESET PASSWORD </div>
+            {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            <br></br>
+        </div>
+    </div>
+);
+}
+
+export default SignUp
